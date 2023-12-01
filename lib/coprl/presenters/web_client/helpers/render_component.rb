9e6c72f8a7135d3d7ff6e0839adc697c906f6a14
Nick Miller <nick@evvnt.com>
2023-12-01 13:25:30 -0500

    fix(web_client): Fix ERB inspection of parents
    The web client's API application loses POM hierarchy when inflating JSON
    to POM. This causes components which rely on checking their parent (or
    an ancestor) for an attribute to exhibit different behavior and render
    differently than the web client's view-rendering (non-API) application.
    
    This change allows templates to pass down local variables (as one
    expects to do with ERB), and updates the places relying on the above
    behavior to use locals instead.

--- lib/coprl/presenters/web_client/helpers/render_component.rb
+++ lib/coprl/presenters/web_client/helpers/render_component.rb
@@ -1,11 +1,12 @@
 module Coprl::Presenters::WebClient::Helpers
   module RenderComponent
-    def render_component(scope, comp, components, index)
+    def render_component(scope, comp, components, index, locals: {})
       Coprl::Presenters::WebClient::ComponentRenderer.new(comp,
                                                           render: method(:render_partial),
                                                           scope: scope,
                                                           components: components,
-                                                          index: index).render
+                                                          index: index,
+                                                          locals: locals).render
     end
   end
 end

09876706e4f900191373645a84d26f5321b5ebcd
Dan Barber <dan@evvnt.com>
2023-12-20 17:52:32 -0600

    Build CSS using Dart Sass directly

--- views/mdc/webpack.config.js
+++ views/mdc/webpack.config.js
@@ -1,63 +1,30 @@
 module.exports = [
   {
-    entry: './assets/scss/app.scss',
-    output: {
-      // This is necessary for webpack to compile
-      // But we never use style-bundle.js
-      filename: '../../public/style-bundle.js',
-    },
-    module: {
-      rules: [{
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '../../public/bundle.css',
-            },
-          },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              // includePaths: ["./node_modules"],
-              // importer: function(url, _prev) {
-              //   if (url.indexOf('@material') === 0) {
-              //     var filePath = url.split('@material')[1];
-              //     var nodeModulePath = `./node_modules/@material/${filePath}`;
-              //     return { file: require('path').resolve(nodeModulePath) };
-              //   }
-              //   return { file: url };
-              // }
-            }
-          }
-        ]
-      }]
-    },
-  },
-  {
     entry: ["./assets/js/app.js"],
     output: {
-      filename: "../../public/bundle.js"
+      filename: "../../../public/bundle.js",
     },
     module: {
-      rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader'
-      }]
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+        },
+      ],
     },
   },
   {
     entry: ["./assets/js/wc.js"],
     output: {
-      filename: "../../public/wc.js"
+      filename: "../../../public/wc.js",
     },
     module: {
-      rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader'
-      }]
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+        },
+      ],
     },
-  }
+  },
 ];

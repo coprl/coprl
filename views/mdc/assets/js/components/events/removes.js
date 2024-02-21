ac3570871a87fd929737ef0cfc044fc12cf33da5
Nick Miller <nick@evvnt.com>
2024-02-21 09:46:51 -0500

    chore(web client): Make `removes` safer
    Previously, the `removes` DSL action would blow up if an element could
    not be found, halting its action chain. Now, a log is emitted and the
    action chain continues.

--- views/mdc/assets/js/components/events/removes.js
+++ views/mdc/assets/js/components/events/removes.js
@@ -6,16 +6,20 @@ export class VRemoves {
         this.root = root;
     }
 
-    call(results) {
-        const ids = this.ids;
-        const root = this.root;
-        return new Promise(function(resolve) {
-            results.push({action: 'removes', statusCode: 200});
-            for (const id of ids) {
-                const elem = root.getElementById(id);
-                elem.parentNode.removeChild(elem);
+    async call(results) {
+        results.push({action: 'removes', statusCode: 200});
+
+        for (const id of this.ids) {
+            const element = this.root.getElementById(id);
+
+            if (element) {
+                element.remove();
             }
-            resolve(results);
-        });
+            else {
+                console.log(`remove: no element with ID "${id}"`);
+            }
+        }
+
+        return results;
     }
 }
